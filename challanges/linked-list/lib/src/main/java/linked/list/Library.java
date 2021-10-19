4423eaade2051aacea53cb0a670fcd44ec0594ea
tamara97.b <tamarabassam1997@icloud.com>
2021-10-19 23:16:57 +0300

    gfdsfdX

--- challanges/linked-list/lib/src/main/java/linked/list/Library.java
+++ challanges/linked-list/lib/src/main/java/linked/list/Library.java
@@ -6,6 +6,9 @@ Public class App {
         linkedList.insert(4);
         linkedList.insert(7);
         linkedList.insert(8);
+        linkedList.kthFromEnd(5);
         System.out.println(linkedList.toString());
-        System.out.println(linkedList.includes(7));}}
+        System.out.println(linkedList.includes(7));}
+
+}
 
