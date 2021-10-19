61f784c6e730956788f6027216eba379f1a7d16b
tamara97.b <tamarabassam1997@icloud.com>
2021-10-19 22:52:33 +0300

    kjhbgv

---
+++ challanges/linked-list/lib/src/main/java/linked/list/Library.java
@@ -0,0 +1,11 @@
+package Class ;
+Public class App {
+    Public static void main (String[] args ){
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        Sysrem.out.println(linkedList.toString());
+        linkedList.insert(4);
+        linkedList.insert(7);
+        linkedList.insert(8);
+        System.out.println(linkedList.toString());
+        System.out.println(linkedList.includes(7));}}
+
